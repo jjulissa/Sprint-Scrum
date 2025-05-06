ed58001dd7b437a40c69e7c159a06f763e0ff6e6
jjulissa <julissajoseph9@gmail.com>
2025-05-06 20:30:55 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-orange-600">
+      <h1 className='text-white'> @ Sprint Scrum </h1>
+    </div>
+  );
+}
+
+export default App;
