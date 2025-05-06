fcceaec29ecef0769601f28e84fc98d71a366949
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-06 20:54:31 +0200

    feat: crear componente Carrito.js

---
+++ src/components/Carrito.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Carrito = () => {
+  return (
+    <div className="p-4 border-t mt-6">
+      <h2 className="text-xl font-bold">Carrito</h2>
+    </div>
+  );
+};
+
+export default Carrito;
