a7fef79581c521926ae0477e7f1e44847791f506
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-07 17:23:03 +0200

    feat: añadir cartProvider al App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import { CartProvider } from './context/CartContext';
 import './App.css';
 
 function App() {
   return (
+    <CartProvider>
     <div className="bg-orange-600">
       <h1 className='text-white'> @ Sprint Scrum </h1>
     </div>
+  </CartProvider>
+
   );
 }
 
